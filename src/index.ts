71d508de8ee7f377cd1cd81acf76d903d250714b
Thibaud RENAUX <thibaud.renaux@gmail.com>
2024-02-28 12:17:33 +0100

    prettier index file

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,27 @@ import jwtDecode from "jwt-decode";
 import nJwt, { Jwt } from "njwt";
 
 import { validIssuer } from "@cryptr/cryptr-config-validation";
-import { CryptrConfig, CryptrOptions, RejectCallback, ResolveCallback, VerifyError } from "./interfaces";
+import {
+  CryptrConfig,
+  CryptrOptions,
+  RejectCallback,
+  ResolveCallback,
+  VerifyError,
+} from "./interfaces";
 import { DEFAULT_OPTS, SIGNING_ALG } from "./defaults";
 
 const genIss = (tnt: string, issuer: string): string => {
-  return `${issuer}/t/${tnt}`
-}
+  return `${issuer}/t/${tnt}`;
+};
 
 const claimsErrors = (claims: any, cryptrConfig: CryptrConfig): object => {
   return {
-    "issuer": claims["iss"] === genIss(claims["tnt"], cryptrConfig.issuer),
-    "client_ids": cryptrConfig.client_ids.includes(claims["cid"]),
-    "audiences": cryptrConfig.audiences.includes(claims["aud"]),
-    "tenants": cryptrConfig.tenants.includes(claims["tnt"])
-  }
-}
+    issuer: claims["iss"] === genIss(claims["tnt"], cryptrConfig.issuer),
+    client_ids: cryptrConfig.client_ids.includes(claims["cid"]),
+    audiences: cryptrConfig.audiences.includes(claims["aud"]),
+    tenants: cryptrConfig.tenants.includes(claims["tnt"]),
+  };
+};
 class CryptrJwtVerifier {
   cryptrConfig: CryptrConfig;
   jwksUri: string;
@@ -30,15 +36,14 @@ class CryptrJwtVerifier {
     this.cryptrOpts = opts;
     this.cryptrConfig = cryptrConfig;
     this.jwksUri = `${cryptrConfig.issuer}/.well-known`;
-
   }
 
-  getKid(token: string): string | never  {
+  getKid(token: string): string | never {
     const decode: object = jwtDecode(token, { header: true });
     return decode["kid"];
   }
-  
-  getTnt(token: string) : string | undefined {
+
+  getTnt(token: string): string | undefined {
     const decode: object = jwtDecode(token);
     return decode["tnt"];
   }
@@ -52,73 +57,94 @@ class CryptrJwtVerifier {
       cacheMaxEntries: 3,
       jwksRequestsPerMinute: this.cryptrConfig.jwksRequestsPerMinute ?? 10,
       rateLimit: true,
-    })
+    });
     return new Promise((resolve, reject) => {
       client.getSigningKey(kid, (err, key: any) => {
-        if(err) {
+        if (err) {
           return reject(err);
         } else {
-          return resolve(key["publicKey"])
+          return resolve(key["publicKey"]);
         }
-      })
-    })
+      });
+    });
   }
 
   handleVerifyError(reject: RejectCallback, error: any) {
-    this.handleVerifyErrorMessage(reject, error.message)
+    this.handleVerifyErrorMessage(reject, error.message);
   }
 
   handleVerifyErrorMessage(reject: RejectCallback, msg: string) {
-    reject({valid: false, errors: msg})
+    reject({ valid: false, errors: msg });
   }
 
-  handleVerifySuccess(verifiedJwt: Jwt, resolve: ResolveCallback, reject: (reason?: any) => void) {
-    const jwtBody = verifiedJwt["body"]
+  handleVerifySuccess(
+    verifiedJwt: Jwt,
+    resolve: ResolveCallback,
+    reject: (reason?: any) => void
+  ) {
+    const jwtBody = verifiedJwt["body"];
 
-    const errorClaims = claimsErrors(jwtBody, this.cryptrConfig)
-    const validClaims = Object.values(errorClaims).every(item => item)
+    const errorClaims = claimsErrors(jwtBody, this.cryptrConfig);
+    const validClaims = Object.values(errorClaims).every((item) => item);
 
-    if(validClaims) {
-      return resolve({valid: true, claims: jwtBody})
+    if (validClaims) {
+      return resolve({ valid: true, claims: jwtBody });
     } else {
-      let keysToCheck: string[] = []
-      Object.keys(errorClaims).forEach(k => {if(!errorClaims[k]) { keysToCheck.push(k) }})
-      return this.handleVerifyErrorMessage(reject, `Non-compliant claims,\nplease check ${keysToCheck.join(', ')}`)
+      let keysToCheck: string[] = [];
+      Object.keys(errorClaims).forEach((k) => {
+        if (!errorClaims[k]) {
+          keysToCheck.push(k);
+        }
+      });
+      return this.handleVerifyErrorMessage(
+        reject,
+        `Non-compliant claims,\nplease check ${keysToCheck.join(", ")}`
+      );
     }
   }
 
-  verifyTokenWithKey(token: string, publicKey: string | Buffer | undefined, resolve: ResolveCallback, reject: (reason?: any) => void) {
+  verifyTokenWithKey(
+    token: string,
+    publicKey: string | Buffer | undefined,
+    resolve: ResolveCallback,
+    reject: (reason?: any) => void
+  ) {
     try {
-      const verifiedJwt = nJwt.verify(token, publicKey, SIGNING_ALG);  
+      const verifiedJwt = nJwt.verify(token, publicKey, SIGNING_ALG);
       if (verifiedJwt !== undefined) {
         return this.handleVerifySuccess(verifiedJwt, resolve, reject);
       } else {
-        return this.handleVerifyErrorMessage(reject, 'unable to verify token')
+        return this.handleVerifyErrorMessage(reject, "unable to verify token");
       }
-    } catch(error) {
-      return this.handleVerifyError(reject, error)
-        }
+    } catch (error) {
+      return this.handleVerifyError(reject, error);
+    }
   }
 
   async verify(token: string): Promise<unknown> {
+    return new Promise(
+      (resolve: (value: any) => void, reject: (reason?: any) => void) => {
+        try {
+          const kid = this.getKid(token)!!!;
+          const tnt = this.getTnt(token)!!!;
 
-    return new Promise((resolve: (value: any) => void, reject: (reason?: any) => void) => {
-      try{
-        const kid = this.getKid(token)!!!;
-        const tnt = this.getTnt(token)!!!;
-
-        this.getPublicKey(tnt, kid)
-          .then(publicKey => {
-            this.verifyTokenWithKey(token, (publicKey as CertSigningKey).toString(), resolve, reject)
-          })
-          .catch((err) => {
-            this.handleVerifyError(reject, err)
-          })
-      } catch(err)
-      {
-        this.handleVerifyError(reject, err as VerifyError)
+          this.getPublicKey(tnt, kid)
+            .then((publicKey) => {
+              this.verifyTokenWithKey(
+                token,
+                (publicKey as CertSigningKey).toString(),
+                resolve,
+                reject
+              );
+            })
+            .catch((err) => {
+              this.handleVerifyError(reject, err);
+            });
+        } catch (err) {
+          this.handleVerifyError(reject, err as VerifyError);
+        }
       }
-    });
+    );
   }
 }
 
